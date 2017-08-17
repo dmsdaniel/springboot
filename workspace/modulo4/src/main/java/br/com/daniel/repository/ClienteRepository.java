289b3ae2fdcdecb3fb6941b8930746d617ce3041
Daniel <dmsdaniel@gmail.com>
2017-08-17 00:07:19 -0300

    notebook casa joinfaces

--- workspace/modulo4/src/main/java/br/com/daniel/repository/ClienteRepository.java
+++ workspace/modulo4/src/main/java/br/com/daniel/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package br.com.daniel.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +11,9 @@ import br.com.daniel.model.Cliente;
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 	@Query("select c from Cliente c where c.email=:email")
 	public Cliente buscarPorEmail(@Param("email") String email) ;
+	
+	@Query("select c from Cliente c")
+	public List<Cliente> buscarTodos();
+		
 	
 }
--- workspace/modulo4/src/test/java/br/com/daniel/repository/TestClienteRepository.java
+++ workspace/modulo4/src/test/java/br/com/daniel/repository/TestClienteRepository.java
@@ -3,6 +3,8 @@ package br.com.daniel.repository;
 import javax.persistence.EntityManager;
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,12 +35,27 @@ public class TestClienteRepository {
 		Assert.assertNotNull(cliSalvo.getId());
 	}
 	
+	@Test
 	public void TestBuscarPorEmail() {
 		Cliente cli = new Cliente("Daniel","dmsdaniel@gmail.com");
 		entityManager.persist(cli);
 		Cliente clienteEncontrado = clienteRepository.buscarPorEmail("dmsdaniel@gmail.com");
 		assertThat(clienteEncontrado.getNome()).isEqualTo(cli.getNome());
 		assertThat(clienteEncontrado.getEmail()).isEqualTo(cli.getEmail());
+		
+	}
+	@Test
+	public void TestBuscarTodos() {
+		Cliente cli = new Cliente("Daniel","dmsdaniel@gmail.com");
+		entityManager.persist(cli);
+		
+		Cliente cli2 = new Cliente("Daniel2","dmsdaniel2@gmail.com");
+		entityManager.persist(cli2);
+		
+		List<Cliente> lista = clienteRepository.buscarTodos();
+		assertThat(lista.get(0).getNome()).isEqualTo(cli.getNome());
+		assertThat(lista.get(1).getNome()).isEqualTo(cli2.getNome());
+		
 		
 	}
 } 
